60e6b2da3b2dd3743ffccfc198a00eb7d1e454b5
Shi Yu Liu <shiyu0811liu@gmail.com>
2023-07-10 21:58:51 -0400

    built Login and Signup components; fine-tuning file uploading functionality and styling

--- client/App.jsx
+++ client/App.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login.jsx";
 import Signup from "./components/Signup.jsx";
+import PasswordReset from './components/PasswordReset.jsx'
+import UserProfile from "./components/UserProfile.jsx";
 // import "./stylesheets/styles.css";
 
 const App = () => {
   return (
     <div>
-      Hello
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/passwordreset" element={<PasswordReset />} />
+          <Route path="/userprofile" element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
