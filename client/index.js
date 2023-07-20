9246b1a5657de658e96ea168da81a6f6763e46ef
Chandler <lcchrty@gmail.com>
2023-07-19 21:03:24 -0700

    redux toolkit installed and store initialized in idex.js

--- client/index.js
+++ client/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { store } from "./store/store.js";
+import { Provider } from "react-redux";
 
 // const domNode = document.getElementById("root");
 // const root = createRoot(domNode);
 // root.render(<App />);
 
-createRoot(document.querySelector('#root')).render(<App/>)
+createRoot(document.querySelector("#root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
